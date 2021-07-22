56325d9c91f217b338a1e1a09f2820b4dfe678c1
primys <primys@usoamic.io>
2021-07-22 20:00:54 +0300

    Add config for SpringFox

---
+++ src/main/kotlin/io/usoamic/app/config/SpringFoxConfig.kt
@@ -0,0 +1,21 @@
+package io.usoamic.app.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@Configuration
+class SpringFoxConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
+            .paths(PathSelectors.any())
+            .build()
+    }
+}
