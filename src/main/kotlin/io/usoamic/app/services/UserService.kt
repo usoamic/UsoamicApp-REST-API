5dacc1cc6e381d19c479d8e2518f796ef6cb8d46
primys <primys@usoamic.io>
2021-07-31 18:43:44 +0300

    Add UserService

---
+++ src/main/kotlin/io/usoamic/app/services/UserService.kt
@@ -0,0 +1,25 @@
+package io.usoamic.app.services
+
+import io.usoamic.app.mappers.UserMapper
+import io.usoamic.app.models.data.UserModel
+import io.usoamic.app.models.data.AddUserModel
+import io.usoamic.app.repositories.UsersRepository
+import org.springframework.stereotype.Service
+import javax.inject.Inject
+
+@Service
+class UserService @Inject constructor(
+    private val usersRepository: UsersRepository,
+    private val userMapper: UserMapper
+){
+    fun getUsers(): List<UserModel> {
+        return usersRepository.findAll()
+            .map(userMapper::dbToModel)
+            .toList()
+    }
+
+    fun addUsers(user: AddUserModel) {
+        val model = userMapper.addUserModelToDb(user)
+        usersRepository.save(model)
+    }
+}
