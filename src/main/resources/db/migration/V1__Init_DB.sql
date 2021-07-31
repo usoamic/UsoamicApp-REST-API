a5fd819b81fc5c09b875b66967521c45d09612d8
primys <primys@usoamic.io>
2021-07-31 18:37:45 +0300

    Update db template

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,9 +1,8 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE SCHEMA IF NOT EXISTS usoamicapp;
 
 CREATE TABLE IF NOT EXISTS usoamicapp.users
 (
-    user_id     uuid DEFAULT uuid_generate_v4(),
+    user_id     uuid NOT NULL,
     private_key VARCHAR NOT NULL,
     phone       VARCHAR NOT NULL,
     PRIMARY KEY (user_id)
