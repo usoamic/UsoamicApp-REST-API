bc3abcd682f7887ad69eb5c90605c32c540bd8b5
primys <primys@usoamic.io>
2021-12-06 01:43:31 +0300

    Add AppTuner

---
+++ src/main/kotlin/io/usoamic/app/utils/AppTuner.kt
@@ -0,0 +1,25 @@
+package io.usoamic.app.utils
+
+import io.usoamic.app.services.AccountService
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class AppTuner(
+    private val accountService: AccountService,
+    @Value("\${usoamic.wallet-password}")
+    private val walletPassword: String,
+) {
+    @PostConstruct
+    fun createAccount() {
+        if (accountService.hasAccount()) {
+            val credentials = accountService.createCredentials()
+
+            accountService.addAccount(
+                password = walletPassword,
+                privateKey = credentials.privateKey
+            )
+        }
+    }
+}
