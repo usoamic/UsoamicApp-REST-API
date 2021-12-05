1a846095e81f99adb0ab6de9dda17e0788b04dab
primys <primys@usoamic.io>
2021-12-06 02:56:20 +0300

    Exclude DB support

--- src/main/kotlin/io/usoamic/app/UsoamicAppRestApiApplication.kt
+++ src/main/kotlin/io/usoamic/app/UsoamicAppRestApiApplication.kt
@@ -1,9 +1,19 @@
 package io.usoamic.app
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
+@EnableAutoConfiguration(
+    exclude = [
+        DataSourceAutoConfiguration::class,
+        HibernateJpaAutoConfiguration::class
+    ]
+)
 class UsoamicAppRestApiApplication
 
 fun main(args: Array<String>) {
